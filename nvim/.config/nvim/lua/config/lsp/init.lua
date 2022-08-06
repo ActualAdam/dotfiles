02a2768f49c07c3e6ec6fdcbdb0dd8b5eecef4f8
Adam Miller <adam@adammiller.name>
2022-08-06 10:26:28 -0700

    remove lsp installer config file, since I inlined it in the lsp init config

--- nvim/.config/nvim/lua/config/lsp/init.lua
+++ nvim/.config/nvim/lua/config/lsp/init.lua
@@ -110,35 +110,4 @@ function init.setup()
     setup(coqd_configs)
 end
 
--- local installer = {}
--- 
--- local lsp_installer_servers = require("nvim-lsp-installer.servers")
--- local utils = require("utils")
--- 
--- function installer.setup(servers, options)
---     for server_name, _ in pairs(servers) do
---         local server_available, server = lsp_installer_servers.get_server(server_name)
--- 
---         if server_available then
---             server:on_ready(function()
---                 local opts = vim.tbl_deep_extend("force", options, servers[server.name] or {})
---                -- server:setup(require("coq").lsp_ensure_capabilities(opts))
---                 server:setup(opts)
---             end)
--- 
---             if not server:is_installed() then
---                 utils.info("Installing " .. server.name)
---                 server:install()
---             end
---         else
---             utils.error(server)
---         end
---     end
--- end
--- 
--- return installer
---
---
-
-
 return init
