360ca2fb79dd482fd436d1a246b23f9a843f7bc1
Adam Miller <adam@adammiller.name>
2022-09-02 15:11:48 -0700

    nvim, coq tabnine

--- nvim/.config/nvim/lua/config/coq.lua
+++ nvim/.config/nvim/lua/config/coq.lua
@@ -1,6 +1,14 @@
 local coq = {}
 
 function coq.setup()
+    vim.g.coq_settings = {
+        auto_start = true,
+        clients = {
+            tabnine = {
+                enabled = true,
+            }
+        }
+    }
     local coq = require("coq")
     coq.Now()
 end
