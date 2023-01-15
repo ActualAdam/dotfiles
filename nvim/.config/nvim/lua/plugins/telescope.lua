45ae19565bac2c18f0bbb4e4fae9a86cf1a75c8b
Adam Miller <adam@adammiller.name>
2023-01-15 08:17:40 -0800

    more plugin breakout

---
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,29 @@
+return {
+  -- Fuzzy Finder (files, lsp, etc)
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    configure = function()
+      -- [[ Configure Telescope ]]
+      -- See `:help telescope` and `:help telescope.setup()`
+      require('telescope').setup {
+        defaults = {
+          mappings = {
+            i = {
+              ['<C-u>'] = false,
+              ['<C-d>'] = false,
+            },
+          },
+        },
+      }
+    end
+  },
+
+  -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+    cond = vim.fn.executable "make" == 1
+  },
+}
